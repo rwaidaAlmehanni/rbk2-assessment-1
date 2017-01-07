e137e95abc0d9f4d960b90401b8df25e4f653b07
rwaida <rwaidaAlmehanni>
2017-01-07 10:08:31 +0200

    last change

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,19 @@
-// your code here
+var Array=function(){
+	var instance={};
+	instance.arr=[];
+	_.extend(instance,arrayMethods);
+	return instance;
+};
+arrayMethods.first=function(){
+	if(this.arr.length>0){
+	return this.arr[0];
+    }
+return "empty array.."; 
+};
+arrayMethods.last=function(){
+	if(this.arr.length>0){
+    var l=this.arr.length-1;
+	return this.arr[l];
+	}
+	return "empty array..";
+};
